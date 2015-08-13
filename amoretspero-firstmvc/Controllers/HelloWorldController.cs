61a0397356b8e35eded7b489fd3425043bdb430c
Jiung Hahm <amoretspero@gmail.com>
2015-08-13 20:43:25 +0900

    ASP.NET MVC 5 Practice - Done to 8.Adding Search

--- amoretspero-firstmvc/Controllers/HelloWorldController.cs
+++ amoretspero-firstmvc/Controllers/HelloWorldController.cs
@@ -15,9 +15,12 @@ namespace amoretspero_firstmvc.Controllers
         }
 
         // GET: /HelloWorld/Welcome/
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", Your ID is : " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
